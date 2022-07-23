059a98e709756aca77ad1460d314f3d60593a47e
talisong-cardoso <talisong.cardoso@gmail.com>
2022-07-23 14:19:22 -0300

    validação dos input adicionadas

--- src/js/index.js
+++ src/js/index.js
@@ -7,40 +7,91 @@ function handleSubmit(event) {
     const lastName = form["last-name"];
     const email = form["email"];
     const password = form["password"];
+    handleFirstName(firstName);
+    handleLastName(lastName);
     handleValidEmail(email);
-    // const paragEmailError = email.nextElementSibling as HTMLParagraphElement;
+    handlePassword(password);
 }
-function handleValidEmail(email) {
-    const checkEmail = isValidEmail(email.value);
-    const parentLabel = email.parentElement;
-    if (typeof checkEmail !== "string") {
-        const errorMessage = parentLabel.children[1];
-        const elemImg = parentLabel.children[2];
-        errorMessage.setAttribute("hidden", "");
-        elemImg.style.display = "none";
+function handleFirstName(input) {
+    handleInputs(input, "First Name cannot be empty");
+}
+function handleLastName(input) {
+    handleInputs(input, "Last Name cannot be empty", 2);
+}
+function handleValidEmail(input) {
+    const checkEmail = isValidEmail(input.value);
+    if (typeof checkEmail === "string") {
+        handleInputs(input, checkEmail);
     }
     else {
+        handleInputs(input, checkEmail);
+    }
+}
+function handlePassword(input) {
+    handleInputs(input, "Password cannot be empty", 6);
+}
+/**
+ * Altera o DOM entre exibir e esconder um erro informativo ao usuário
+ * @param input Elemento input a ser verificado.
+ * @param textError  Texto personalizado que será exibido se o input for inválido.
+ * @param limitCharacter Limete de caracteres necessários para a validação do input.
+ */
+function handleInputs(input, textError, limitCharacter = 3) {
+    const id = input.id;
+    const errorMessage = document.querySelector(`#${id} + p`);
+    const iconError = document.querySelector(`#${id} ~ img`);
+    const showError = () => {
+        const parentElement = input.parentElement;
         const errorMessage = document.createElement("p");
-        const img = document.createElement("img");
-        errorMessage.textContent = checkEmail;
-        img.src = "./src/assets/images/icon-error.svg";
-        img.classList.add("claim-form__icon-error");
-        img.style.display = "block";
-        if (parentLabel.childElementCount <= 1) {
-            email.insertAdjacentElement("afterend", errorMessage);
+        const iconError = document.createElement("img");
+        errorMessage.textContent = textError;
+        errorMessage.id = `${id}-error`;
+        iconError.src = "./src/assets/images/icon-error.svg";
+        iconError.alt = "icon error";
+        iconError.classList.add("claim-form__icon-error");
+        parentElement.append(errorMessage, iconError);
+        input.setAttribute("aria-invalid", "true");
+        input.setAttribute("aria-errormessage", `${id}-error`);
+    };
+    const hiddenError = () => {
+        errorMessage === null || errorMessage === void 0 ? void 0 : errorMessage.remove();
+        iconError === null || iconError === void 0 ? void 0 : iconError.remove();
+        input === null || input === void 0 ? void 0 : input.setAttribute("aria-invalid", "false");
+        input.removeAttribute("aria-errormessage");
+    };
+    if (input.id == "email") {
+        if (typeof isValidEmail(input.value) == "string") {
+            if (!document.querySelector("#email-error")) {
+                showError();
+            }
         }
-        if (parentLabel.childElementCount <= 2) {
-            errorMessage.insertAdjacentElement("afterend", img);
+        else {
+            console.log("else do email");
+            hiddenError();
+        }
+    }
+    else {
+        if (!input.value || input.value.length < limitCharacter) {
+            if (!errorMessage) {
+                showError();
+            }
+        }
+        else {
+            hiddenError();
         }
     }
 }
 function isValidEmail(email) {
     return !email
-        ? "Field is empty"
+        ? "Email cannot be empty"
         : !testEmail(email)
             ? "Looks like this is not an email"
             : true;
 }
+/**
+ * Retorna um valor Boolean se uma string corresponde ao padrão de um email ou não. `(regex feita por mim, qualquer falha de verificação reporte.)`.
+ * @param email Uma string qualquer que será analisado se é um email.
+ */
 function testEmail(email) {
     const regEx = /^\w(\w\.?)+\w+@\w+(\.\w+)*(\w?)$/;
     return regEx.test(email);
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,46 +9,103 @@ function handleSubmit(event: Event) {
   const email = form["email"] as HTMLInputElement;
   const password = form["password"] as HTMLInputElement;
 
+  handleFirstName(firstName);
+  handleLastName(lastName);
   handleValidEmail(email);
+  handlePassword(password);
 }
 
-function handleValidEmail(email: HTMLInputElement) {
-  const checkEmail = isValidEmail(email.value);
-  const parentLabel = email.parentElement as HTMLLabelElement;
+function handleFirstName(input: HTMLInputElement): void {
+  handleInputs(input, "First Name cannot be empty");
+}
 
-  if (typeof checkEmail !== "string") {
-    const errorMessage = parentLabel.children[1] as HTMLParagraphElement;
-    const elemImg = parentLabel.children[2] as HTMLImageElement;
+function handleLastName(input: HTMLInputElement): void {
+  handleInputs(input, "Last Name cannot be empty", 2);
+}
 
-    errorMessage.setAttribute("hidden", "");
-    elemImg.style.display = "none";
+function handleValidEmail(input: HTMLInputElement) {
+  const checkEmail = isValidEmail(input.value);
+  if (typeof checkEmail === "string") {
+    handleInputs(input, checkEmail);
   } else {
+    handleInputs(input, checkEmail);
+  }
+}
+
+function handlePassword(input: HTMLInputElement) {
+  handleInputs(input, "Password cannot be empty", 6);
+}
+/**
+ * Altera o DOM entre exibir e esconder um erro informativo ao usuário
+ * @param input Elemento input a ser verificado.
+ * @param textError  Texto personalizado que será exibido se o input for inválido.
+ * @param limitCharacter Limete de caracteres necessários para a validação do input.
+ */
+function handleInputs(
+  input: HTMLInputElement,
+  textError: string | boolean,
+  limitCharacter: number = 3
+) {
+  const id = input.id;
+  const errorMessage = document.querySelector(
+    `#${id} + p`
+  ) as HTMLParagraphElement;
+  const iconError = document.querySelector(`#${id} ~ img`) as HTMLImageElement;
+
+  const showError = () => {
+    const parentElement = input.parentElement as HTMLLabelElement;
     const errorMessage = document.createElement("p");
-    const img = document.createElement("img");
+    const iconError = document.createElement("img");
+    errorMessage.textContent = textError as string;
+    errorMessage.id = `${id}-error`;
+    iconError.src = "./src/assets/images/icon-error.svg";
+    iconError.alt = "icon error";
+    iconError.classList.add("claim-form__icon-error");
+    parentElement.append(errorMessage, iconError);
+    input.setAttribute("aria-invalid", "true");
+    input.setAttribute("aria-errormessage", `${id}-error`);
+  };
 
-    errorMessage.textContent = checkEmail;
-    if (parentLabel.childElementCount <= 1) {
-      email.insertAdjacentElement("afterend", errorMessage);
+  const hiddenError = () => {
+    errorMessage?.remove();
+    iconError?.remove();
+    input?.setAttribute("aria-invalid", "false");
+    input.removeAttribute("aria-errormessage");
+  };
+
+  if (input.id == "email") {
+    if (typeof isValidEmail(input.value) == "string") {
+      if (!document.querySelector("#email-error")) {
+        showError();
+      }
+    } else {
+      console.log("else do email");
+      hiddenError();
     }
-
-    img.src = "./src/assets/images/icon-error.svg";
-    img.classList.add("claim-form__icon-error");
-    img.style.display = "block";
-    if (parentLabel.childElementCount <= 2) {
-      errorMessage.insertAdjacentElement("afterend", img);
+  } else {
+    if (!input.value || input.value.length < limitCharacter) {
+      if (!errorMessage) {
+        showError();
+      }
+    } else {
+      hiddenError();
     }
   }
 }
 
-function isValidEmail(email: string) {
+function isValidEmail(email: string): string | boolean {
   return !email
-    ? "Field is empty"
+    ? "Email cannot be empty"
     : !testEmail(email)
-      ? "Looks like this is not an email"
-      : true;
+    ? "Looks like this is not an email"
+    : true;
 }
 
-function testEmail(email: string) {
+/**
+ * Retorna um valor Boolean se uma string corresponde ao padrão de um email ou não. `(regex feita por mim, qualquer falha de verificação reporte.)`.
+ * @param email Uma string qualquer que será analisado se é um email.
+ */
+function testEmail(email: string): boolean {
   const regEx = /^\w(\w\.?)+\w+@\w+(\.\w+)*(\w?)$/;
   return regEx.test(email);
 }
